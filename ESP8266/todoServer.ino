92410d2a683dbf75ac51a08ec39d17a823f520d5
Rick <garzag.rick@gmail.com>
2018-09-27 12:09:50 -0500

    Create todoServer.ino

---
+++ ESP8266/todoServer.ino
@@ -0,0 +1,248 @@
+/*
+  FSWebServer - Example WebServer with SPIFFS backend for esp8266
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WebServer library for Arduino environment.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  upload the contents of the data folder with MkSPIFFS Tool ("ESP8266 Sketch Data Upload" in Tools menu in Arduino IDE)
+  or you can upload the contents of a folder if you CD in that folder and run the following command:
+  for file in `ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/edit; done
+
+  access the sample web page at http://esp8266fs.local
+  edit the page by going to http://esp8266fs.local/edit
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>
+
+#define DBG_OUTPUT_PORT Serial
+
+const char *ssid = "rasp";
+const char *password = "s3rv1c10$";
+
+ESP8266WebServer server(80);
+//holds the current upload
+File fsUploadFile;
+
+bool ledOn = false;
+
+//format bytes
+String formatBytes(size_t bytes)
+{
+    if (bytes < 1024)
+    {
+        return String(bytes) + "B";
+    }
+    else if (bytes < (1024 * 1024))
+    {
+        return String(bytes / 1024.0) + "KB";
+    }
+    else if (bytes < (1024 * 1024 * 1024))
+    {
+        return String(bytes / 1024.0 / 1024.0) + "MB";
+    }
+    else
+    {
+        return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+    }
+}
+
+String getContentType(String filename)
+{
+    if (server.hasArg("download"))
+    {
+        return "application/octet-stream";
+    }
+    else if (filename.endsWith(".htm"))
+    {
+        return "text/html";
+    }
+    else if (filename.endsWith(".html"))
+    {
+        return "text/html";
+    }
+    else if (filename.endsWith(".css"))
+    {
+        return "text/css";
+    }
+    else if (filename.endsWith(".js"))
+    {
+        return "application/javascript";
+    }
+    else if (filename.endsWith(".png"))
+    {
+        return "image/png";
+    }
+    else if (filename.endsWith(".gif"))
+    {
+        return "image/gif";
+    }
+    else if (filename.endsWith(".jpg"))
+    {
+        return "image/jpeg";
+    }
+    else if (filename.endsWith(".ico"))
+    {
+        return "image/x-icon";
+    }
+    else if (filename.endsWith(".xml"))
+    {
+        return "text/xml";
+    }
+    else if (filename.endsWith(".pdf"))
+    {
+        return "application/x-pdf";
+    }
+    else if (filename.endsWith(".zip"))
+    {
+        return "application/x-zip";
+    }
+    else if (filename.endsWith(".gz"))
+    {
+        return "application/x-gzip";
+    }
+    return "text/plain";
+}
+
+bool handleFileRead(String path)
+{
+    DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+    if (path.endsWith("/"))
+    {
+        path += "index.html";
+    }
+    String contentType = getContentType(path);
+    String pathWithGz = path + ".gz";
+    if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+    {
+        if (SPIFFS.exists(pathWithGz))
+        {
+            path += ".gz";
+        }
+        File file = SPIFFS.open(path, "r");
+        server.streamFile(file, contentType);
+        file.close();
+        return true;
+    }
+    return false;
+}
+
+void handleFileList()
+{
+    if (!server.hasArg("dir"))
+    {
+        server.send(500, "text/plain", "BAD ARGS");
+        return;
+    }
+
+    String path = server.arg("dir");
+    DBG_OUTPUT_PORT.println("handleFileList: " + path);
+    Dir dir = SPIFFS.openDir(path);
+    path = String();
+
+    String output = "[";
+    while (dir.next())
+    {
+        File entry = dir.openFile("r");
+        if (output != "[")
+            output += ',';
+        bool isDir = false;
+        output += "{\"type\":\"";
+        output += (isDir) ? "dir" : "file";
+        output += "\",\"name\":\"";
+        output += String(entry.name()).substring(1);
+        output += "\"}";
+        entry.close();
+    }
+
+    output += "]";
+    server.send(200, "text/json", output);
+}
+
+void setup(void)
+{
+    DBG_OUTPUT_PORT.begin(115200);
+    DBG_OUTPUT_PORT.print("\n");
+    DBG_OUTPUT_PORT.setDebugOutput(true);
+    SPIFFS.begin();
+    {
+        Dir dir = SPIFFS.openDir("/");
+        while (dir.next())
+        {
+            String fileName = dir.fileName();
+            size_t fileSize = dir.fileSize();
+            DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+        }
+        DBG_OUTPUT_PORT.printf("\n");
+    }
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        DBG_OUTPUT_PORT.print(".");
+    }
+    DBG_OUTPUT_PORT.println("");
+    DBG_OUTPUT_PORT.print("Connected! IP address: ");
+    DBG_OUTPUT_PORT.println(WiFi.localIP());
+
+    //called when the url is not defined here
+    //use it to load content from SPIFFS
+    server.onNotFound([]() {
+        if (!handleFileRead(server.uri()))
+        {
+            server.send(404, "text/plain", "FileNotFound");
+        }
+    });
+
+    pinMode(LED_BUILTIN, OUTPUT); // declare LED as output
+
+    //list directory
+    server.on("/list", HTTP_GET, handleFileList);
+
+    server.on("/changeLed", HTTP_GET, []() {
+        if (ledOn)
+        {
+            digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on (Note that LOW is the voltage level
+            ledOn = !ledOn;
+        }
+        else
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+            ledOn = !ledOn;
+        }
+    });
+
+    //get heap status, analog input value and all GPIO statuses in one json call
+    server.on("/all", HTTP_GET, []() {
+        String json = "{";
+        json += "\"heap\":" + String(ESP.getFreeHeap());
+        json += ", \"analog\":" + String(analogRead(A0));
+        json += ", \"gpio\":" + String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
+        json += "}";
+        server.send(200, "text/json", json);
+        json = String();
+    });
+
+    server.begin();
+    DBG_OUTPUT_PORT.println("HTTP server started");
+}
+
+void loop(void)
+{
+    server.handleClient();
+}
